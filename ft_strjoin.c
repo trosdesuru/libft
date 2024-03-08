e2af742d50101451353853b594e7bf1097f517a0
Eduard Hernandez Ventos <edhernan@cbr6s5.42barcelona.com>
2024-03-08 10:35:35 +0100

    Improved code | version 2.0

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: edhernan <edhernan@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 17:39:13 by edhernan          #+#    #+#             */
-/*   Updated: 2024/03/08 10:28:21 by edhernan         ###   ########.fr       */
+/*   Updated: 2024/03/08 10:35:08 by edhernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	1*ft_strjoin(char const *strdest, char const *strjoin)
 	size_t	i;
 	size_t	j;
 
-	str = ft_calloc(sizeof(char), ft_strlen(s1) + ft_strlen(s2) + 1);
+	str = ft_calloc(sizeof(char), ft_strlen(strdest) + ft_strlen(strjoin) + 1);
 	if (!str)
 		return (NULL);
 	i = 0;
